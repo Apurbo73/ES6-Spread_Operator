84bd42a7dae9b4912bf69327158b1e0a51772822
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-11 03:15:39 +0600

    done

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 //Spread Operator:
 var numbers = [1, 2, 3];
 var newArray = [...numbers, 4, 5, 6];
-console.log(newArray);
+numbers.push(10);
+// console.log(newArray); //output is:[ 1, 2, 3, 4, 5, 6 ]
+//
+
+//***concat using spread operator */
+var a = [1, 2, 3];
+var b = [4, 5, 6];
+var result = [...a, ...b];
+console.log(result);
+//output is:[ 1, 2, 3, 4, 5, 6 ]
