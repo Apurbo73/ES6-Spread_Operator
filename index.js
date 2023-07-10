87935f22e1bdeb58d6d35958c8c7e9588ba65e1e
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-11 03:19:53 +0600

    done

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ numbers.push(10);
 // console.log(newArray); //output is:[ 1, 2, 3, 4, 5, 6 ]
 //
 
-//***concat using spread operator */
+//***concat array using spread operator */
 var a = [1, 2, 3];
 var b = [4, 5, 6];
 var result = [...a, ...b];
-console.log(result);
-//output is:[ 1, 2, 3, 4, 5, 6 ]
+console.log(result); //output is:[ 1, 2, 3, 4, 5, 6 ]
+
+//***concat object using spread operator */
+var object1 = {
+  a: 1,
+  b: 2
+};
+var object2 = {
+  c: 3,
+  d: 4
+};
+var object3 = { ...object1, ...object2 };
+console.log(object3); // output is : { a: 1, b: 2, c: 3, d: 4 }
